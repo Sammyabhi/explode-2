cb3eaae6ef3472e4f2d4f107472320924d511d9e
Abhishek Kumar Yadav <87541023+Sammyabhi@users.noreply.github.com>
2023-03-17 09:55:32 +0530

    committed

---
+++ src/Components/Technical Details/Button.jsx
@@ -0,0 +1,53 @@
+import "../Configurator/style.css";
+import "../../index.css";
+import { useStore } from "../../store/store";
+const Button = () => {
+  return (
+    <div className="configurator">
+      {/* <div className="Configurator_section_title">Color</div> */}
+
+      <div className="configurator__section">
+        <div className="btnContainer">
+          <button
+            onClick={() => {
+              //
+              useStore.setState({
+                openExplode: !useStore.getState().openExplode,
+              });
+            }}
+          >
+            Explode
+          </button>
+        </div>
+        <div className="btnContainer">
+          <button
+            className="configurator__section__title"
+            onClick={() => {
+              //
+              useStore.setState({
+                openDimension: !useStore.getState().openDimension,
+              });
+            }}
+          >
+            Dimension
+          </button>
+        </div>
+        <div className="btnContainer">
+          <button
+            className="configurator__section__title"
+            onClick={() => {
+              //
+              useStore.setState({
+                openSpeedmeeter: !useStore.getState().openSpeedmeeter,
+              });
+            }}
+          >
+            Statistics
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
